10fd713c6fd6acc7b0b0b23947541c1e3a13e9a7
krom~ <akrommuhammadyusuf@gmail.com>
2026-01-04 21:34:40 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+long add_numbers(long a, long b);
+
+int main() {
+    long hasil = add_numbers(10, 32);
+    printf("hasil dari assembly adalah: %ld\n", hasil);
+    return 0;
+}    
